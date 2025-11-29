79901b6d182752a950492f2d5e8a941bc0b6ea8c
Aniket0025 <aniketyadav25012005@gmail.com>
2025-11-29 11:29:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./components/Navbar"
+import About from "./sections/About"
+import Contact from "./sections/Contact"
+import Experience from "./sections/Experience"
+import Footer from "./sections/Footer"
+import Home from "./sections/Home"
+import Project from "./sections/Project"
+import Skills from "./sections/Skills"
+import Testimonials from "./sections/Testimonials"
+
+function App() {
+
+  return (
+    <div className="relative gradient text-white">
+
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Experience/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CustomCurdor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomCurdor = () => {
+  return (
+    <div>CustomCurdor</div>
+  )
+}
+
+export default CustomCurdor
